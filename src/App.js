c490afbac66dba165173d21033a60e88b7323d83
Demetrius <wh46tesmoke@gmail.com>
2021-10-14 16:41:42 +0300

    lesson2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React, {useState} from 'react'
 import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
-function App() {
-    const [likes, setlikes] = useState(5);
-    const [value, setValue] = useState('input text')
-    console.log(likes);
-    console.log(setlikes);
+import './styles/App.css';
+import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
 
+function App() {
+    const [posts, setPosts] = useState([
+        {id:1, title: 'JavaScript', body:'Description'},
+        {id:2, title: 'React', body:'Description'},
+        {id:3, title: 'Jquery', body:'Description'},
+
+    ])
   return (
     <div className="App">
-        <Counter/>
-        <ClassCounter/>
+        <PostList posts={posts} title="Список постов 1"/>
     </div>
   );
 }
